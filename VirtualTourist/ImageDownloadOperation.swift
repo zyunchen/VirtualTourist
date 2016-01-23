83ef0a866c6f1a92aaded9fcc1c6a7b868344c39
zhangyunchen <nail@mrzozo.com>
2016-01-23 16:19:15 +0800

    complete the project for review

---
+++ VirtualTourist/ImageDownloadOperation.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDownloadOperation.swift
+//  VirtualTourist
+//
+//  Created by zhangyunchen on 16/1/21.
+//  Copyright © 2016年 zhangyunchen. All rights reserved.
+//
+
+import Foundation
---
+++ VirtualTourist/ImageDownloadQueue.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDownloadQueue.swift
+//  VirtualTourist
+//
+//  Created by zhangyunchen on 16/1/22.
+//  Copyright © 2016年 zhangyunchen. All rights reserved.
+//
+
+import Foundation
